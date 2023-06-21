37f178a53aeb3cf2d24ed03027c55707617c9084
hesen_ch75 <hesen_ch75@STHQ0118-05>
2023-06-21 08:42:01 +0400

    Add project files.

---
+++ StoreWithAsync/Views/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using StoreWithAsync.Views;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace StoreWithAsync
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Product_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = 0;
+            var obj = myDataGrid.SelectedItem;
+            var selectedPro = obj as DataRowView;
+
+            if (selectedPro != null)
+            {
+                Id = int.Parse(selectedPro.Row.ItemArray[0].ToString());
+            }
+
+            if (obj is null)
+                MessageBox.Show("Productlardan birini secin, zehmet olmasa", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+            {
+                UpdateWindow window = new UpdateWindow(Id);
+                window.ShowDialog();
+            }
+        }
+
+        private async void Show_Product_Click(object sender, RoutedEventArgs e)
+        {
+            // using (var conn = new SqlConnection())
+            // {
+            //     conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            //     DataSet set = new DataSet();
+            //
+            //     var da = new SqlDataAdapter("Select * From Products", conn);
+            //
+            //     da.Fill(set,"Products");
+            //     DataViewManager dvm= new DataViewManager(set);
+            //     dvm.DataViewSettings["Products"].RowFilter = "UnitPrice<100";
+            //     DataView dataView = dvm.CreateDataView(set.Tables["Products"]);
+            //
+            //
+            //     myDataGrid.ItemsSource= dataView;
+            // 
+            // }
+
+
+            using (var conn = new SqlConnection())
+            {
+                conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+                conn.Open();
+
+                SqlCommand command = conn.CreateCommand();
+                command.CommandText = "WAITFOR DELAY '00:00:03';";
+                command.CommandText += "Select * From Products";
+
+
+                var table = new DataTable();
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    do
+                    {
+                        var hasColumnAdded = false;
+                        while (await reader.ReadAsync())
+                        {
+                            if (!hasColumnAdded)
+                            {
+                                hasColumnAdded = true;
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    table.Columns.Add(reader.GetName(i));
+                                }
+                            }
+
+                            var row = table.NewRow();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[i] = await reader.GetFieldValueAsync<Object>(i);
+                            }
+                            table.Rows.Add(row);
+
+
+                        }
+                    } while (reader.NextResult());
+
+                    myDataGrid.ItemsSource = table.DefaultView;
+                }
+            }
+        }
+
+
+
+        private void Delete_Product_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = 0;
+            var obj = myDataGrid.SelectedItem;
+            var selectedPro = obj as DataRowView;
+
+            if (selectedPro != null)
+                Id = int.Parse(selectedPro.Row.ItemArray[0].ToString());
+
+            if (obj is null)
+                MessageBox.Show("Productlardan birini secin, zehmet olmasa", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+            {
+                using (var conn = new SqlConnection())
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+                    conn.Open();
+                    DataSet dataSet = new DataSet();
+
+                    SqlCommand command = new SqlCommand("Select * From Products", conn);
+
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.Fill(dataSet, "Products");
+
+                    command = new SqlCommand($"Delete From Products Where ProductId={Id}", conn);
+                    dataAdapter.UpdateCommand = command;
+                    dataAdapter.Update(dataSet, "Products");
+                    dataAdapter.UpdateCommand.ExecuteNonQuery();
+
+
+
+                    dataAdapter.Fill(dataSet, "Products");
+                }
+            }
+        }
+    }
+}
---
+++ StoreWithAsync/Views/UpdateWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Data;
+using System.Windows;
+using System.Configuration;
+using System.Data.SqlClient;
+using System;
+using System.Text;
+
+namespace StoreWithAsync.Views
+{
+
+    public partial class UpdateWindow : Window
+    {
+        public int Id { get; set; }
+        public UpdateWindow(int id)
+        {
+            InitializeComponent();
+
+            Id = id;
+        }
+
+        private void Update_Product_Clikc(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+
+            if (string.IsNullOrWhiteSpace(product_name_txt.Text))
+            {
+                sb.Append("ProductName i daxil edin,zehmet olmasa\n");
+                count++;
+            }
+            if (string.IsNullOrWhiteSpace(unit_price_txt.Text))
+            {
+                sb.Append("Unit Price i daxil edin,zehmet olmasa\n");
+                count++;
+            }
+            if (string.IsNullOrWhiteSpace(discon_txt.Text))
+            {
+                sb.Append("Discontinued i daxil edin,zehmet olmasa\n");
+                count++;
+            }
+
+            if (count > 0)
+                MessageBox.Show(sb.ToString(), "Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else
+            {
+                using (var conn = new SqlConnection())
+                {
+                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["myConnString"].ConnectionString;
+                    conn.Open();
+
+                    DataSet set = new DataSet();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+                    SqlCommand sqlCommand = new SqlCommand("Select * From Products", conn);
+                    dataAdapter.SelectCommand = sqlCommand;
+                    dataAdapter.Fill(set, "Products");
+
+                    // Id
+                    var paramId = new SqlParameter();
+                    paramId.ParameterName = "@productId";
+                    paramId.SqlDbType = SqlDbType.Int;
+                    paramId.Value = Id;
+
+                    // productName
+                    var paramName = new SqlParameter();
+                    paramName.ParameterName = "@productName";
+                    paramName.SqlDbType = SqlDbType.NVarChar;
+                    paramName.Value = product_name_txt.Text;
+
+                    // unitPrice
+                    var paramPrice = new SqlParameter();
+                    paramPrice.ParameterName = "@unitPrice";
+                    paramPrice.SqlDbType = SqlDbType.Money;
+                    paramPrice.Value = unit_price_txt.Text;
+
+                    // quantity
+
+                    var paramDiscon = new SqlParameter();
+                    paramDiscon.ParameterName = "@discon";
+                    paramDiscon.SqlDbType = SqlDbType.Bit;
+                    int netice = int.Parse(discon_txt.Text.ToString());
+                    bool result = Convert.ToBoolean(netice);
+                    paramDiscon.Value = result;
+
+                    sqlCommand = new SqlCommand("UpdateProduct", conn);
+
+
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add(paramName);
+                    sqlCommand.Parameters.Add(paramId);
+                    sqlCommand.Parameters.Add(paramDiscon);
+                    sqlCommand.Parameters.Add(paramPrice);
+
+                    dataAdapter.UpdateCommand = sqlCommand;
+                    dataAdapter.Update(set, "Products");
+                    dataAdapter.UpdateCommand.ExecuteNonQuery();
+
+                    MessageBox.Show("Succesfully Update", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                DialogResult = true;
+            }
+        }
+    }
+}
